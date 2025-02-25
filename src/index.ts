9a079e84c3180392a53fa2311f5d49c251715945
corner_coder <56599360+fatihohn@users.noreply.github.com>
2025-02-25 22:41:42 +0900

    src/index.ts: catch error and send error mail

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import cron from "node-cron";
 import { ApiClient } from "./lib/api-client";
 import { JobCollectorDaemon } from "./daemon/job-collector.daemon";
 import { DB } from "./lib/database";
+import { Mailer } from "./lib/mailer";
 
 const config = JSON.parse(readFileSync(`${__dirname}/config.json`, "utf-8"));
 const { rootURL, organizations, locations, keywords } = config;
 const apiClient = new ApiClient(rootURL);
+const mailer = Mailer.getInstance();
+
 const jobCollectorDaemon = new JobCollectorDaemon(
   apiClient,
   new DB(`${__dirname}/data/database.sqlite`),
@@ -24,8 +27,16 @@ cron.schedule("0 * * * *", () => {
       try {
         await jobCollectorDaemon.collectJobs();
         isDone = true;
+        console.error("Job collection complete", new Date());
       } catch (error) {
         console.error("Job collection failed", error);
+        await mailer.sendEmail(
+          process.env.GMAIL_USER,
+          "Job collection failed",
+          "Job collection failed",
+          error.message
+        );
+        await new Promise((resolve) => setTimeout(resolve, 60000));
       }
     }
   });
